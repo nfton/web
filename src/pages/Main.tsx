29bd04c9bf11bf78e74761b4968b00ee49b64510
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-09-30 18:42:55 +0200

    test twa

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,38 @@
 import {Button} from "@mui/material";
 import {Link} from "react-router-dom";
+import {HapticFeedback, ready, Viewport, WebApp} from 'twa-client-sdk';
+import {useEffect} from "react";
 
 export function Main() {
+	useEffect(() => {
+		// @ts-ignore
+		window.Telegram.WebApp.ready()
+		// @ts-ignore
+		window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
+	}, [])
+	const error = () => {
+		// @ts-ignore
+		window.Telegram.WebApp.HapticFeedback.notificationOccurred('error')
+	}
+	const warning = () => {
+		// @ts-ignore
+		window.Telegram.WebApp.HapticFeedback.notificationOccurred('warning')
+	}
+	const success = () => {
+		// @ts-ignore
+		window.Telegram.WebApp.HapticFeedback.notificationOccurred('success')
+	}
+	const changed = () => {
+		// @ts-ignore
+		window.Telegram.WebApp.HapticFeedback.selectionChanged()
+	}
 	return (<div>
 		<Button component={Link} variant="contained" color="primary" to="/app">To App</Button>
-		<Button component={Link} variant="contained" color="secondary" to="/app">To App</Button>
+		<Button variant="contained" color="secondary" onClick={error}>ERROR</Button>
+		<Button variant="contained" color="secondary" onClick={success}>SUCCESS</Button>
+		<Button variant="contained" color="secondary" onClick={warning}>WARNING</Button>
+		<Button variant="contained" color="secondary" onClick={changed}>CHANGED</Button>
+		{	/* @ts-ignore */}
+		<code>{JSON.stringify(window.Telegram.WebApp.themeParams)}</code>
 	</div>)
 }
