cc3f6a1c4e6bcd4bb3ae6a2d245e4d2fa528d945
y.a <asanovich.y@gmail.com>
2022-09-30 20:25:50 +0200

    header logic - level and balance

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,10 @@
 // Imports
 //----------------------------------------------------------------------------------------------------------------------
 
-import React from 'react'
+import React, { memo, useCallback } from 'react'
 import { AppBar, IconButton, Toolbar } from '@mui/material'
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
 import { useAction, useTypedSelector } from '../../hooks'
 import { ECoin } from '../../types'
 import nextId from 'react-id-generator'
@@ -16,15 +17,30 @@ import './header.scss'
 // Header Component
 //----------------------------------------------------------------------------------------------------------------------
 
-export const Header: React.FC = () => {
+export const Header: React.FC = memo(() => {
 
 	const { currentCoin, gameAmount, tonAmount } = useTypedSelector(state => state.coins)
+	const { level } = useTypedSelector(state => state.player)
 	const { switchCurrentCoin } = useAction()
+
+	const handleLevelOrderWindow = useCallback(() => {
+
+	}, [ ])
 
 	return (
 		<AppBar className="header" position="static" id={ nextId('header-') }>
 			<Toolbar className="header-tools">
-				<div>TODO</div>
+				<div className="level">
+					<IconButton
+						id={ nextId('header-') }
+						className="arrow"
+						aria-label="delete"
+						onClick={ handleLevelOrderWindow }
+					>
+					<ArrowCircleUpIcon />
+					</IconButton>
+					<span className="value">{level}</span>
+				</div>
 				<div className="balance">
 					<span className="amount">{ currentCoin === ECoin.TON ? tonAmount.toFixed(2) : gameAmount }</span>
 					<IconButton
@@ -34,10 +50,10 @@ export const Header: React.FC = () => {
 						color="default"
 						onClick={ switchCurrentCoin }
 					>
-						{ currentCoin === ECoin.TON ? iconTon : <MonetizationOnIcon/> }
+						{ currentCoin === ECoin.TON ? iconTon : <MonetizationOnIcon /> }
 					</IconButton>
 				</div>
 			</Toolbar>
 		</AppBar>
 	)
-}
+})
