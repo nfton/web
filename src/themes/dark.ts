f1f2713b3b09f4066249fb751676731f8a7b28ba
y.a <asanovich.y@gmail.com>
2022-09-30 18:53:25 +0200

    favicon and temp theme colors

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -3,10 +3,21 @@ import {createTheme} from "@mui/material";
 export const darkTheme = createTheme({
 	palette: {
 		primary: {
-			main: '#7c81a5',
+			main: '#19B76B',
+			light: '#bafdd8',
+			dark: '#11683e',
+			contrastText: '#ffffff',
 		},
 		secondary: {
-			main: '#3b0318',
+			main: '#f50057',
+		},
+		background: {
+			default: '#1a1a1a',
+			paper: '#333333',
+		},
+		text: {
+			secondary: '#a1a1a1',
+			disabled: '#b3b3b3',
 		},
 	}
 });
--- src/themes/light.ts
+++ src/themes/light.ts
@@ -3,10 +3,22 @@ import {createTheme} from "@mui/material";
 export const lightTheme = createTheme({
 	palette: {
 		primary: {
-			main: '#3f51b5',
+			main: '#19B76B',
+			light: '#bafdd8',
+			dark: '#11683e',
+			contrastText: '#ffffff',
 		},
 		secondary: {
 			main: '#f50057',
+		},
+		background: {
+			default: '#eeeeee',
+			paper: '#ffffff',
+		},
+		text: {
+			secondary: '#a1a1a1',
+			disabled: '#b3b3b3',
+			primary: '#1a1a1a',
 		},
 	}
 });
