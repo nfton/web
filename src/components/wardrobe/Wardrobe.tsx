b3eaa769b90f2f4444c0f30706ce7d34f7da9b4c
Arina Momot <arinamomot@gmail.com>
2022-10-02 01:06:10 +0200

    fix height

--- src/components/wardrobe/Wardrobe.tsx
+++ src/components/wardrobe/Wardrobe.tsx
@@ -3,28 +3,33 @@ import React, {CSSProperties, memo} from "react";
 import {theme} from "../../themes";
 import {Box, Tab, Tabs, Typography} from "@mui/material";
 import {EAttributes} from "../../types";
+import {useWindowDimensions} from "../../hooks";
 
 export const Wardrobe: React.FC = memo(() => {
+    const {height} = useWindowDimensions()
+
     const divStyle: CSSProperties = {
+        height: (height - (height * 0.4) - 80 - 70) + 'px',
         backgroundColor: theme.palette.background.paper
     }
 
-    const [value, setValue] = React.useState('hats');
+    const [value, setValue] = React.useState('cardigan');
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
 
     return <div style={divStyle} className="wardrobe-container">
-        <Box sx={{ width: '100%' }}>
+        <Box>
             <Tabs
                 value={value}
                 onChange={handleChange}
                 textColor="secondary"
                 indicatorColor="secondary"
                 aria-label="tabs"
+                centered
             >
-                <Tab value="hats" label={EAttributes.CARDIGAN} />
+                <Tab value="cardigan" label={EAttributes.CARDIGAN} />
                 <Tab value="collar" label={EAttributes.COLLAR} />
                 <Tab value="pants" label={EAttributes.PANTS} />
                 <Tab value="t-shirt" label={EAttributes.T_SHIRT} />
