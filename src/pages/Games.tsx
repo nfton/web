cab86e6a2f294a5c05e5420592c46f706435c003
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-10-01 15:12:39 +0200

    removed action

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,6 +1,6 @@
-import React, {memo} from "react";
+import React from "react";
 import {Game} from "../types";
-import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
+import {Card, CardContent, CardMedia, Typography} from "@mui/material";
 import {theme} from "../themes";
 import "./games.scss"
 
@@ -59,23 +59,21 @@ const GamesPage: React.FC = () => {
 				))}
 			</div>
 			<Typography color={theme.palette.text.disabled} variant="h6" align={"center"}
-			            sx={{paddingTop: '10px'}}>Singleplayer</Typography>
+			            sx={{paddingTop: '10px'}}>SinglePlayer</Typography>
 			<div className={'games-cards games-cards__multiplayer'}>
 				{games.filter(e => e.players === 1).map(e => (
 					<Card key={e.type} variant={"outlined"}>
-						<CardActionArea>
-							<CardMedia
-								component="img"
-								height={100}
-								image={e.image}
-								alt={e.type}
-							/>
-							<CardContent sx={{padding: '10px 10px 3px 10px'}}>
-								<Typography gutterBottom variant="h6" component="div">
-									{e.type}
-								</Typography>
-							</CardContent>
-						</CardActionArea>
+						<CardMedia
+							component="img"
+							height={100}
+							image={e.image}
+							alt={e.type}
+						/>
+						<CardContent sx={{padding: '10px 10px 3px 10px'}}>
+							<Typography gutterBottom variant="h6" component="div">
+								{e.type}
+							</Typography>
+						</CardContent>
 					</Card>
 				))}
 			</div>
