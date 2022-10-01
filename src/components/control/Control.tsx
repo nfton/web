d66117945e586dff091f4ffd7823a3992c42bb5f
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-10-01 10:48:25 +0200

    button is visible on desktop

--- src/components/control/Control.tsx
+++ src/components/control/Control.tsx
@@ -6,18 +6,11 @@ import "./control.scss"
 export const Control: React.FC = () => {
 	const {height} = useWindowDimensions();
 	// @ts-ignore
-	const divHeight = height - (window.Telegram.WebApp?.viewportStableHeight || 800)
+	const divHeight = Math.max(height - (window.Telegram.WebApp?.viewportStableHeight || 0), 250)
 	const divStyle: CSSProperties = {
-		height: divHeight + 'px',
-		position: 'fixed',
-		bottom: 0,
-		width: '100%',
-		display: 'flex',
-		justifyContent: "center",
-		paddingTop: '65px',
-		border: '1px solid red',
+		height: divHeight + 'px'
 	}
-	return <div style={divStyle}>
-		<Button variant="contained" size="large" className="control-play">Play</Button>
+	return <div style={divStyle} className="control-container">
+		<Button variant="contained" size="large" className="control-play" color="primary">Play</Button>
 	</div>
 }
