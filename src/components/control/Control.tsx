bee28d5e158ade44601b9740f539bf7b66654e2e
y.a <asanovich.y@gmail.com>
2022-10-01 13:49:31 +0200

    footer to index

--- src/components/control/Control.tsx
+++ src/components/control/Control.tsx
@@ -1,21 +1,22 @@
-import React, {CSSProperties, memo} from "react";
-import useWindowDimensions from "../../hooks/useWindowDimensions";
-import {Button} from "@mui/material";
+import React, { CSSProperties, memo } from "react";
+import { Button } from "@mui/material";
+import useWindowDimensions from '../../hooks/useWindowDimensions'
+
 import "./control.scss"
 import {useNavigate} from "react-router-dom";
 import {EPages} from "../../types";
 
 export const Control: React.FC = memo(() => {
-	const {height} = useWindowDimensions();
+	const { height } = useWindowDimensions()
 	const navigate = useNavigate()
 
 	// @ts-ignore
-	const divHeight = Math.max(height - (window.Telegram.WebApp?.viewportStableHeight || 0), 250)
+	const divHeight = Math.max(height - (window.Telegram.WebApp?.viewportStableHeight || 0) - 80, 250)
 	const divStyle: CSSProperties = {
 		height: divHeight + 'px'
 	}
 	return <div style={divStyle} className="control-container">
-		<Button variant="contained" size="large" className="control-play" color="primary"
+		<Button variant="contained" size="large" className="button" color="primary"
 		        onClick={() => navigate(EPages.GAMES)}>Play</Button>
 	</div>
 })
